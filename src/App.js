abfe7a2798389e6b9826e35d49161cf0d0768af4
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2022-12-22 23:23:17 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast}/>}
+      {forecast && <Forecast data={forecast }/>}
     </div>
   );
 }
