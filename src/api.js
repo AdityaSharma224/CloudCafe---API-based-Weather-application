d93bbef2477e8e6991a4c8276f333020afea2078
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2022-12-22 22:59:30 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1bffb6e9d8msh9b911963b7f2578p1865b2jsn5a0b545cc2e3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "719a8786e6c64a4e98588904fed32d09"
+
